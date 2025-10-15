e432a2b9d8d7ce0ed9dac0e686ecd4b5647eab4e
saran625 <sraj62588@gmail.com>
2025-10-15 15:11:13 +0530

    change the output

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Jenkins CI/CD Pipeline!"
+    return "Hello, Jenkins CI/CD Pipeline changes will be saved!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
